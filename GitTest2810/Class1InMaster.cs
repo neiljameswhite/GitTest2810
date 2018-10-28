8788405f20840bd56d79fdf87fa9d2bec3a9a9b6
neil <neil@NEILWHITED88B>
2018-10-28 18:30:57 +0000

    Add project files.

---
+++ GitTest2810/Class1InMaster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest2810
+{
+    class Class1InMaster
+    {
+        public static void AddedInMaser()
+        {
+
+        }
+    }
+}
